cf745a02535548334e16d01f5a6641220714d1d8
hari kishan <kishanviit13@gmail.com>
2022-08-02 08:11:58 +0530

    if conditons

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -8,10 +8,21 @@ fi
 
 echo -e "\e[31m Installing Nginx \e[0m"
 yum install nginx -y
+if [ $? -eq 0 ]; then
+  echo  -e "\e[31mSUCCESS\e[0m"
+else
+  echo "\e[32mFAILURE\e[0m"
+  exit 1
+fi
 
 echo -e "\e[32m Downloding Nginx \e[0m"
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
-
+if [ $? -eq 0 ]; then
+  echo  -e "\e[31mSUCCESS\e[0m"
+else
+  echo "\e[32mFAILURE\e[0m"
+  exit 2
+fi
 echo -e "\e[33m Cleanup Old Nginx Content and Extract New Downloded Archive \e[0m"
 
 rm -rf /usr/share/nginx/html/*
@@ -21,7 +32,12 @@ mv frontend-main/* .
 mv static/* .
 rm -rf frontend-main README.md
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
-
+if [ $? -eq 0 ]; then
+  echo  -e "\e[31mSUCCESS\e[0m"
+else
+  echo "\e[32mFAILURE\e[0m"
+  exit 2
+fi
 echo -e "\e[34m Starting Nginx \e[0m"
 
 systemctl restart nginx
