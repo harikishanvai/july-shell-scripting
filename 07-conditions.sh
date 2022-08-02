c3081ef4686d775feea8c632100b756953064cea
hari kishan <kishanviit13@gmail.com>
2022-08-02 07:29:44 +0530

    if conditons

--- 07-conditions.sh
+++ 07-conditions.sh
@@ -16,3 +16,21 @@ then
   echo HELLO
 fi
 
+# Expressions are important
+# 1. String Tests
+# Operators : ==, !* , -z
+# 2. Number Tests
+# 3. File Test
+
+a="abc"
+if [ $a == "abc" ]; then
+  echo Both are equal
+fi
+
+if [ $a !* "abc" ]; then
+  echo "Both are not equal"
+fi
+
+if [ -z "$b" ]; then
+  echo b variable is empty
+fi
