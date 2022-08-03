3746dbe845e5d6d7d20330c14a7a2ac6b9ce9c1b
hari kishan <kishanviit13@gmail.com>
2022-08-03 06:29:59 +0530

    functions

---
+++ 08-functions.sh
@@ -0,0 +1,21 @@
+#!/bib/bash
+
+# variable  : if we assign a name to set of data that is a variable
+# function : if we assign a name to set of commands that is a function
+
+# func_name() {
+# commands
+# commands
+# }
+
+## declare a function
+
+Print_Message() {
+  echo Hello
+  echo world
+  echo name
+}
+
+Print_Message
+
+## Note , Function has to declare
