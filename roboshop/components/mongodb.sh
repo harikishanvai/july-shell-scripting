39763829a2340da1262de6d96f9a36838e255df5
hari kishan <kishanviit13@gmail.com>
2022-08-26 06:46:35 +0530

    mongodb setup

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -13,21 +13,7 @@ StatCheck $?
 Print "Update MongoDB Listen Address"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 StatCheck $?
+
 Print "Start MongoDB"
-systemctl enable mongod &>>$LOG_FILE && systemctl start mongod &>>$LOG_FILE
-
-
-Download the schema and load it.
-
-```
-# curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
-
-# cd /tmp
-# unzip mongodb.zip
-# cd mongodb-main
-# mongo < catalogue.js
-# mongo < users.js
-
-```
-
-Symbol `<` will take the input from a file and give that input to the command.
+systemctl enable mongod &>>$LOG_FILE && systemctl restart mongod &>>$LOG_FILE
+StatCheck $?
