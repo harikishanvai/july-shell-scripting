723a8496d66aa641ea1e96cba50901c211d1e732
hari kishan <kishanviit13@gmail.com>
2022-08-26 07:45:08 +0530

    catalouge setup

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -11,8 +11,11 @@ yum install nodejs gcc-c++ -y &>>${LOG_FILE}
 StatCheck $?
 
 Print "Add Apllication User"
+id ${APP_USER} &>>${LOG_FILE}}
+if [ $? -ne 0 ]; then
 useradd ${APP_USER} &>>${LOG_FILE}
 StatCheck $?
+fi
 
 Print "Download app content"
 curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>${LOG_FILE}
